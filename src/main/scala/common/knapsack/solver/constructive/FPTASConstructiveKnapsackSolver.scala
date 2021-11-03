a00e30e1be5ee2f32984e3c278039b2d54b81687
zbyju <zbynek.jurica@gmail.com>
2021-11-03 19:41:21 +0100

    Add solver for FPTAS dynamic programming

---
+++ src/main/scala/common/knapsack/solver/constructive/FPTASConstructiveKnapsackSolver.scala
@@ -0,0 +1,23 @@
+package cz.cvut.fit.juriczby
+package common.knapsack.solver.constructive
+import common.knapsack.instance.contructive.{ConstructiveInstance, ConstructiveResult}
+import common.stats.StatsTracker
+
+import cz.cvut.fit.juriczby.common.knapsack.instance.Item
+
+class FPTASConstructiveKnapsackSolver extends AbstractConstructiveKnapsackSolver {
+  override def solve(i: ConstructiveInstance, statsTracker: StatsTracker): ConstructiveResult = {
+    val dpcSolver = new DPPriceConstructiveKnapsackSolver()
+    val itemsThatFit = i.items.filter(_.weight <= i.knapsackCapacity)
+    if(itemsThatFit.isEmpty) return ConstructiveResult(0, i.chosenItems, statsTracker)
+
+    val maxPrice = itemsThatFit.maxBy(_.price).price
+    val epsilon = 0.001
+    val K = Math.min((epsilon * maxPrice) / i.numberOfItems, 1)
+    val FPTASInstance = ConstructiveInstance(i.chosenItems, i.name, i.id, i.numberOfItems, i.knapsackCapacity, i.items.map(i => Item(i.weight, Math.floor(i.price / K).toInt)))
+
+    val dpcRes = dpcSolver.solve(FPTASInstance, new StatsTracker())
+
+    ConstructiveResult(Math.round(dpcRes.maxPrice * K).toInt, dpcRes.chosenItems, statsTracker)
+  }
+}
